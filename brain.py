0105b51791a6d217cc11ae01ebe8289e17d2ddf9
wn <ansyu@deloitte.com.hk>
2022-05-09 00:19:01 +0800

    update file

--- brain.py
+++ brain.py
@@ -17,29 +17,15 @@ class Brain():
           #Creating the neural network
           self.model = Sequential()
           self.model.trainable = True
-          self.model.add(Dense(units = 32, activation = 'relu', input_shape = (self.numInputs, )))
+          self.model.add(Dense(units = 32, activation = 'softmax', input_shape = (self.numInputs, )))
           
-          self.model.add(Dense(units = 32, activation = 'relu'))
+          self.model.add(Dense(units = 16, activation = 'softmax'))
 
-          self.model.add(Dense(units = 16, activation = 'relu'))
-
-          self.model.add(Dense(units = 16, activation = 'relu'))
-
-          self.model.add(Dense(units = 8, activation = 'relu'))
-
-          self.model.add(Dense(units = 8, activation = 'relu'))
-
-          self.model.add(Dense(units = 4, activation = 'relu'))
+          self.model.add(Dense(units = 8, activation = 'softmax'))
           
-          self.model.add(Dense(units = 4, activation = 'relu'))
-          
-          self.model.add(Dense(units = 4, activation = 'relu'))
-          
-          self.model.add(Dense(units = 4, activation = 'relu'))
+          self.model.add(Dense(units = 4, activation = 'softmax'))
 
-          self.model.add(Dense(units = 4, activation = 'relu'))
-
-          self.model.add(Dense(units = self.numOutputs))
+          self.model.add(Dense(units = self.numOutputs,activation='softmax'))
           
           self.model.compile(optimizer = Adam(lr = self.learningRate), loss = 'mean_squared_error')
 
