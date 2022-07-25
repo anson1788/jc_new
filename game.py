eca9bc476fe4ebd2531cbd054cb06ae0a0ecdff5
wn <ansyu@deloitte.com.hk>
2022-07-25 21:48:43 +0800

    aa

--- game.py
+++ game.py
@@ -2,7 +2,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from twocaptcha import TwoCaptcha
 import sys
 import urllib.request
 import os
